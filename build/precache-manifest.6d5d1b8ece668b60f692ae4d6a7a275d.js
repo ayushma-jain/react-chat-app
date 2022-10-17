ba7f3be9febf926418159ea004d2442e7cdc3841
ayushma-jain <ayushmaj46@gmail.com>
2022-10-17 15:06:37 +0530

    Firebase setup issue fixed

---
+++ build/precache-manifest.6d5d1b8ece668b60f692ae4d6a7a275d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a43719cb6ed1a5788130",
+    "url": "/static/css/main.b035205a.chunk.css"
+  },
+  {
+    "revision": "a43719cb6ed1a5788130",
+    "url": "/static/js/main.8b34d22d.chunk.js"
+  },
+  {
+    "revision": "c5cd4c5a860304b33c51",
+    "url": "/static/js/runtime~main.0aac2fb3.js"
+  },
+  {
+    "revision": "4ff0f7a4b877521d05d1",
+    "url": "/static/js/2.88050971.chunk.js"
+  },
+  {
+    "revision": "5656dea25dd962128fb3",
+    "url": "/static/js/3.cb9903fc.chunk.js"
+  },
+  {
+    "revision": "4433653abc2c5cb49224a775fad7f93b",
+    "url": "/static/media/beauty.4433653a.jpg"
+  },
+  {
+    "revision": "c5ecc4957a9343217c0edf365bca329f",
+    "url": "/index.html"
+  }
+];
